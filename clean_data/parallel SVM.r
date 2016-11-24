6b8b93fd01f8bf35a9a779bb096808b845c970ca
Nilesh Pharate <nileshph@users.noreply.github.com>
2016-11-23 21:47:54 -0600

    Add files via upload

--- clean_data/parallel SVM.r
+++ clean_data/parallel SVM.r
@@ -1,5 +1,6 @@
 
 library(parallelSVM)
+library(doParallel)
 
 train <- read.csv("C:/Users/nileshpharate/Documents/GitHub/ML-Project/data/clean_data/training.csv",na.strings=c("", "NA"))
 test  <- read.csv("C:/Users/nileshpharate/Documents/GitHub/ML-Project/data/normalized/testing.csv",na.strings=c("", "NA"))
@@ -12,7 +13,12 @@ train <- na.omit(train)
 
 train <- train[!apply(is.na(train) | train == " ", 1, all),]
 
-
+cores<-detectCores()
+#Create cluster with desired number of cores, leave one open for the machine         
+#core processes
+cl <- makeCluster(cores[1]-1)
+#Register cluster
+registerDoParallel(cl)
 
 x1<- subset(train, select = -status_group)
 y<- train$status_group
